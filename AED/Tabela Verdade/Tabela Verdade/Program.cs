2ae495692c4ca221d9fd7bbe043c9e6ba2af4063
Elias Flavio De Paiva <1077336@sga.pucminas.br>
2018-03-03 11:57:07 -0300

    Acréscimo do exercício de tabela verdade

---
+++ AED/Tabela Verdade/Tabela Verdade/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabela_Verdade
+{
+    class Program
+    {
+        public static void imprimeVetor(int i, int[] vet)
+        {
+            if (i == vet.Length)
+            {
+                Console.Write("\n|");
+                for (int j = 0; j < vet.Length; j++)
+                    Console.Write(vet[j] + "|");
+            }
+            else
+            {
+                vet[i] = 0;
+                imprimeVetor(i + 1, vet);
+                vet[i] = 1;
+                imprimeVetor(i + 1, vet);
+            }                            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de variáveis você deseja: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[n];
+
+            imprimeVetor(0, vet);
+
+            Console.ReadKey();
+        }
+    }
+}
