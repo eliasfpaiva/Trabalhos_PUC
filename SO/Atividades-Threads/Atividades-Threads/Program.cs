63b26f91e8110b16d8a1ce8ccb04be8dc86c3bd4
Elias Flavio De Paiva <1077336@sga.pucminas.br>
2018-03-08 22:26:12 -0300

    Atividades práticas de Sistemas Operacionais em C++

---
+++ SO/Atividades-Threads/Atividades-Threads/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+class ThreadIdentidade
+{
+    /*
+     * O programa executa e imprime as frases com o id de cada thread 
+     * fica claro que elas são invocadas fora da ordem 
+     * se a intenção fosse executar alguma coisa de forma ordenada, esse não seria um bom recurso
+     * O melhor é aplicar só para o que não importar a ordem de execução.
+    */
+    private int id = 0;
+    public ThreadIdentidade(int id)
+    {
+        this.id = id;
+    }
+    public void ApresentarThread()
+    {
+        Console.WriteLine("Olá mundo, eu sou o thread " + id);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        int numThreads = 20;
+        Thread[] pool = new Thread[numThreads];
+        for (int i = 0; i < numThreads; i++)
+        {
+            ThreadIdentidade b = new ThreadIdentidade(i);
+            pool[i] = new Thread(b.ApresentarThread);
+        }
+        for (int i = 0; i < numThreads; i++)
+            pool[i].Start();
+
+        Console.ReadKey();
+    }
+}
