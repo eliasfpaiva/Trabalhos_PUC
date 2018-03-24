30a6c9000dab885f5a5aa5603e37df9a7291debe
Elias Flavio De Paiva <1077336@sga.pucminas.br>
2018-03-24 11:37:53 -0300

    Início da atividade de filas de AED

---
+++ AED/Fila/Fila/Classes/Aviao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fila.Classes
+{
+    class Aviao
+    {
+        String prefixo;
+
+        public Aviao(String prefixo)
+        {
+            this.prefixo = prefixo;
+        }
+    }
+}
