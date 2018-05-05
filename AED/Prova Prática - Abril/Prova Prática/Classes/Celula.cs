842aa26e31d1d0c4205fb38bf12314edf3240370
Elias Flavio De Paiva <1077336@sga.pucminas.br>
2018-05-05 11:25:43 -0300

    Subindo prova prática de AED

---
+++ AED/Prova Prática - Abril/Prova Prática/Classes/Celula.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_Prática.Classes
+{
+    public class Celula
+    {
+        public int dado;
+        public Celula ant, prox;
+        
+        public Celula()
+        {
+            this.ant = this.prox = null;
+        }
+        public Celula(int dado)
+        {
+            this.dado = dado;
+            this.ant = this.prox = null;
+        }
+    }
+}
---
+++ AED/Prova Prática - Abril/Prova Prática/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prova_Prática.Classes;
+
+namespace Prova_Prática
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista listaComandos = new Lista();
+            string comando;
+            string[] linhaComando;
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Digite o comando: ");
+                comando = Console.ReadLine();
+                linhaComando = comando.Split(' ');
+
+                if (linhaComando[0] == "q")
+                    comando = "q";
+                if (linhaComando[0] == "f")
+                    listaComandos.insere(int.Parse(linhaComando[1]));
+                //if(linhaComando[0] == )
+
+            } while (comando != "q");
+
+            listaComandos.imprime();
+
+            Console.ReadKey();
+        }
+    }
+}
