1992e7fb22ccf32f4bf2ef249e324bb339ced502
Elias Flavio De Paiva <1077336@sga.pucminas.br>
2018-07-02 22:05:38 -0300

    Subindo projeto de .NET Core da Escola de férias

---
+++ Escola de Férias/WebApplication1/WebApplication1/Models/Contato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Contato
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Campo Obrigatório!")]
+        public string Nome { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        public string Celular { get; set; }
+    }
+}
